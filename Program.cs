ce8eb213c4dcacce8f5af964f7a5e399281fe35e
Дима <shilnikovdmitrievich@yandex.ru>
2026-07-12 23:50:39 +0300

    второй

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace DiplomPrototype
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
